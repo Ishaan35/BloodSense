63426f591bc06b394dd119cc180c33c2861cd5e9
Ishaan35 <toishaanpatel@gmail.com>
2024-03-31 13:19:51 -0400

    Specified port in db configuration

--- db/postgresDb.js
+++ db/postgresDb.js
@@ -16,6 +16,7 @@ if (inProduction) {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 5432,
   };
 } else {
   poolConfig = {
